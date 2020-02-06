7ed39790fe3833e07ed44ddd2d1492251a9d5010
IkarosKun <merle.liukun@gmail.com>
2020-02-06 23:14:33 +0800

    feat(weather): :sparkles: 天气爬虫

---
+++ scripts/weather.py
@@ -0,0 +1,67 @@
+"""
+    墨迹天气爬虫
+"""
+from typing import Optional, List
+from dataclasses import dataclass
+
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://tianqi.moji.com/weather/"
+AREA = "china/beijing/chaoyang-district"
+
+
+@dataclass
+class Weather:
+    day: str
+    wea: str
+    temp: str
+    wind: str
+    wind_level: str
+    air: str
+
+
+def get_weather_html() -> Optional[BeautifulSoup]:
+    try:
+        resp = requests.get(BASE_URL + AREA)
+        soup = BeautifulSoup(resp.text, "lxml")
+        return soup
+    except Exception as e:
+        print(f"Exception in get html. errors: {e}")
+        return None
+
+
+def get_tips(soup: BeautifulSoup) -> str:
+    return soup.select(".left > .wea_tips.clearfix > em")[0].get_text()
+
+
+def get_days_wea(soup: BeautifulSoup) -> List[Weather]:
+    days_forecast: List[BeautifulSoup] = soup.select(".wrap.clearfix")[2].select(
+        ".left > .forecast.clearfix > .days.clearfix"
+    )
+
+    data_set = []
+    for day_f in days_forecast:
+        day = day_f.select("li")[0].get_text().strip()
+        wea = day_f.select("li")[1].get_text().strip()
+        temp = day_f.select("li")[2].get_text().strip()
+        wind = day_f.select("li")[3].select("em")[0].get_text()
+        wind_level = day_f.select("li")[3].select("b")[0].get_text()
+        air = day_f.select("li")[4].get_text().strip()
+
+        data_set.append(Weather(
+            day=day,
+            wea=wea,
+            temp=temp,
+            wind=wind,
+            wind_level=wind_level,
+            air=air
+        ))
+
+    return data_set
+
+
+if __name__ == '__main__':
+    html = get_weather_html()
+    print(get_tips(html))
+    print(get_days_wea(html))
