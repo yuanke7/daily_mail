23e98acb80f60e8589161a75a64cfd424f618329
Ikaros <merle.liukun@gmail.com>
2020-04-23 00:14:38 +0800

    feat(crontab):

--- crontab.sh
+++ crontab.sh
@@ -1,1 +1,8 @@
-1 8 * * * python run.py >> /dev/null
+# auto email
+30 7 * * * python run.py >> /dev/null
+
+# stop now docker
+20 7 * * * ./scripts/service_start.sh >> /dev/null
+
+# start stopped docker
+40 7 * * * ./scripts/service_start.sh >> /dev/null
