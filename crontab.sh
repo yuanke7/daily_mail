5aaf11db0fb305d8323e13090c5dc5b2d153f4df
Ikaros <merle.liukun@gmail.com>
2020-04-27 22:28:58 +0800

    fix(codeo):

--- crontab.sh
+++ crontab.sh
@@ -1,8 +1,8 @@
 # auto email
-30 7 * * * python run.py >> /dev/null
+30 7 * * * python run.py > /dev/null 2>&1 &
 
 # stop now docker
-20 7 * * * ./scripts/service_start.sh >> /dev/null
+20 7 * * * ./scripts/service_stop.sh > /dev/null 2>&1 &
 
 # start stopped docker
-40 7 * * * ./scripts/service_start.sh >> /dev/null
+5 12 * * * ./scripts/service_start.sh > /dev/null 2>&1 &
