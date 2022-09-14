5fbe62b8868b6393711a571562d9909656c1f40e
server195 <yuankq1@lenoivo.com>
2022-09-14 11:08:10 +0800

    add loguru as logger

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 """
-    配置
+    config
 """
 
 sentry_dsn = ""
@@ -9,6 +9,7 @@ email_password = ""
 receiver = ""
 sender_name = ""
 receiver_name = ""
+constellation = ''
 
 NICK_NAME = []  # 随机挑选昵称作为标题
 
@@ -21,7 +22,6 @@ CONSTELLATION_MAP = {'白羊': 'Aries', '金牛': 'Taurus', '双子': 'Gemini',
                      '巨蟹': 'Cancer', '狮子': 'Leo', '处女': 'Virgo',
                      '天秤': 'Libra', '天蝎': 'Scorpio', '射手': 'Sagittarius',
                      '摩羯': 'Capricorn', '水瓶': 'Aquarius', '双鱼': 'Pisces'}
-constellation = ''
 XINGZUOWU_URL = "https://www.xzw.com/fortune/"
 ONE_URL = "http://wufazhuce.com/"
 
