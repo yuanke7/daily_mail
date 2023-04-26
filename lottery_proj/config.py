adb971b07cb6b50f8bb70ffc70b38bfba80a4c2f
server195 <yuankq1@lenoivo.com>
2023-04-26 15:58:50 +0800

    lottery.py

--- lottery_proj/config.py
+++
@@ -1,4 +0,0 @@
-receivers = [
-    'ykq12313@gmail.com',
-    '2918591380@qq.com' # xianyu xgldxg
-]
--- lottery_proj/run.py
+++ lottery_proj/run.py
@@ -1,22 +1,20 @@
 import datetime
-
 import zmail
 from loguru import logger
-
-from lottery_proj.config import receivers
+from local_settings import receivers, sender, pwd
 from lottery_proj.lottery import Lottery
 
 
 ins = Lottery(high_probability=False)
 today = datetime.datetime.today().strftime('%Y-%m-%d')
-logger.add(today)
+logger.add('log/' + today+'.log')
 mail = {
-    'subject': f'大乐透推荐号码 {today}',  # Anything you want.
+    'subject': f'大乐透推荐号码 {today}',
     'content_text': f'今天是{today}, 为您推荐以下号码，祝您早日中大奖，发大财！\n\n {ins.recommand_number(2)}',
 }
 
-server = zmail.server('yuanke7@foxmail.com',
-                      'ewppwarmavnzhiad',
+server = zmail.server(sender,
+                      pwd,
                       smtp_host="smtp.qq.com",
                       smtp_port=465)
 
